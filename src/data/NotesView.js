e9d039bf759a9f99042afd7512831952a7a94045
Akira <mcboyhd.de@gmail.com>
2022-09-12 23:22:46 +0200

    First v wit a bit of javascript

---
+++ src/data/NotesView.js
@@ -0,0 +1,34 @@
+export default class NotesView {
+constructor(root, { onNoteSelect, onNoteAdd, onNoteEdit, onNoteDelete } = {}) {
+        this.root = root;
+        this.onNoteSelect = onNoteSelect;
+        this.onNoteAdd = onNoteAdd;
+        this.onNoteEdit = onNoteEdit;
+        this.onNoteDelete = onNoteDelete;
+        this.root.innerHTML = `
+            <div class="notes__sidebar">
+                <button class="cssbuttons-io notes__add">
+                <span>
+                    <i class="material-icons">add</i>
+                        <path d="M0 0h24v24H0z" fill="none"></path>
+                        <path d="M24 12l-5.657 5.657-1.414-1.414L21.172 12l-4.243-4.243 1.414-1.414L24 12zM2.828 12l4.243 4.243-1.414 1.414L0 12l5.657-5.657L7.07 7.757 2.828 12zm6.96 9H7.66l6.552-18h2.128L9.788 21z" fill="currentColor"></path>
+                    </svg> Add note
+                </span>
+                </button>
+                <div class="notes__list"></div>
+            </div>
+            <div class="notes__preview">
+                <input class="notes__title" type="text" placeholder="New Note...">
+                <textarea class="notes__body">Take Note...</textarea>
+            </div>
+        `;
+
+        const btnAddNote = this.root.querySelector(".notes__add");
+        const inpTitle = this.root.querySelector(".notes__title");
+        const inpBody = this.root.querySelector(".notes__body");
+
+        btnAddNote.addEventListener("click", () => {
+            this.onNoteAdd();
+        });
+    }
+}
