df9517ee5287f870c1ff764d5f3ab66611801bd1
akira <mcboyhd.de@gmail.com>
2022-09-22 11:39:13 +0200

    some js

--- src/data/main.js
+++ src/data/main.js
@@ -1,21 +1,57 @@
 import NotesView from "./NotesView.js";
+import NotesAPI from "./NotesAPI.js";
 const theme__btn = document.getElementById("theme");
+const tooglelayout = document.getElementById("layoutcheckbox");
+const layouticon = document.getElementById("layouticon");
 const app =  document.getElementById("app");
 const body = document.getElementById("body");
-const view = new NotesView(app, {
+const layout = document.getElementById("layout");
+const layoutbtn =  document.getElementById("layoutbtn").innerHTML;
+
+const view = new NotesView(app, layoutbtn,{
     onNoteAdd() {
         console.log("Note has been selected");
     },
+
+    onNoteEdit(newTitle, newBody) {
+        console.log(newTitle)
+        console.log(newBody)
+    },
+    onNoteSelect(id) {
+        console.log("Note Selected:" + id);
+    },
+    onNoteDelete(id) {
+        console.log("Note Deleted:" + id);
+    },
 });
+
+view.updateNoteList(NotesAPI.getAllNotes());
+
+
+
+
+
+
+
+
 
 theme__btn.addEventListener("change", () => {
     if(body.classList.contains("white")) {
-        console.log("checked");
         body.className = "dark";
     } else {
-        console.log("unchecked");
         body.className = "white";
     }
-    console.log("Theme has changed");
 });
+
+tooglelayout.addEventListener("change", () => {
+    if(layout.classList.contains("extended")) {
+        layout.className = "compact";
+        layouticon.className = "right"; 
+    } else {
+        layout.className = "extended";
+        layouticon.className = "left";
+    }
+});
+
+
 
